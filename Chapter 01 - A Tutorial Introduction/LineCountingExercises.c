55cfc1efd6af59c7f7fd8c87c0c606ae9bec90c6
João Gabriel Peixoto Rodrigues <contato.joaogabriel@proton.me>
2024-04-13 19:55:36 -0300

    Started the Line Counter Exercises.
    Created Placeholder Functions;

---
+++ Chapter 01 - A Tutorial Introduction/LineCountingExercises.c
@@ -0,0 +1,21 @@
+// © https://www.github.com/archgabs 13-04-2024, 07:30PM
+
+// Exercise 1-8. Write a program to count blanks, tabs, and newlines.
+// Exercise 1-9. eeeeeea program to copy its input to its output, replacing each string of one or  more blanks by a single blank.
+// Exercise 1-10. Write a program to copy its input to its output, replacing each tab by \t, each  backspace by \b, and each backslash by \\. This makes tabs and backspaces visible in an  unambiguous way. 
+
+#include <stdio.h>
+
+int blankTabsNewlinesCounter(){
+	return 0;
+}
+
+int multipleBlanksToOne(){
+	return 0;
+}
+int tabBackspaceBackslashToUnicode(){
+	return 0;
+}
+int main(){
+	return 0;
+}
